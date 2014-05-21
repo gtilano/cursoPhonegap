47a9a457028b79b5201c87fcb0cfe7478dc64ab8
Carlos Rojas <carlos.rojas@falconlabs.co>
2014-05-21 08:45:22 -0500

    se anexo plugin de geoposicionamiento

--- phonegapexample/platforms/android/ant-build/classes.dex.d
+++ phonegapexample/platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,13 @@ C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classe
 C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\adminnova\cursophonegap\R$string.class \
 C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\adminnova\cursophonegap\R$xml.class \
 C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\adminnova\cursophonegap\R.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook$1.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook$2.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook$3.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook$4.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook$5.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook$6.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook$7.class \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\classes\com\ccsoft\plugin\CordovaFacebook.class \
 C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\dexedLibs\classes-306c68ddff90193caea4824d9e710fe3.jar \
+C:\xampp\htdocs\cursoPhonegap\phonegapexample\platforms\android\ant-build\dexedLibs\simple facebook-25505d123219f712ae4147615edcf19e.jar \
--- phonegapexample/platforms/android/assets/www/cordova_plugins.js
+++ phonegapexample/platforms/android/assets/www/cordova_plugins.js
@@ -2,9 +2,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "org.apache.cordova.geolocation": "0.3.7",
-    "com.ccsoft.plugin.CordovaFacebook": "1.0.0"
-}
+{}
 // BOTTOM OF METADATA
 });
